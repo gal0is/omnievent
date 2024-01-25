3394a66ec4733cf47e8108ee282df0aa12df31be
gal0is <zhipeng.job@hotmail.com>
2024-01-25 11:05:42 +0800

    add scripts

---
+++ examples/ED/run-ed-sl-bert-base-chinese.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=$1 python sequence_labeling.py \
+    ../../config/ace2005-zh/ed/sl/bert-base-chinese.yaml
---
+++ examples/ED/run-ed-tc-bert-base-cls-chinese.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=$1 python token_classification.py \
+    ../../config/ace2005-zh/ed/tc/bert-base-chinese.yaml
+    
